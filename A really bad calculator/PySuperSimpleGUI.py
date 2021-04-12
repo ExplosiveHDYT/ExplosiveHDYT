e6d8a9bed31bab3013e9b7b9ac135e319f8d78e6
ExplosiveHDYT <82344578+ExplosiveHDYT@users.noreply.github.com>
2021-04-12 15:55:51 -0400

    Add files via upload

---
+++ A really bad calculator/PySuperSimpleGUI.py
@@ -0,0 +1,12 @@
+from PySimpleGUI import Text, Window, Button, WIN_CLOSED, InputText, Submit, popup, Input, theme
+theme("Black")
+
+
+def PySuperSimpleGUI_instructions():
+    print("Welcome to PySuperSimpleGUI, where you get all the nececarry things from PySimpleGUI to create basic apps and also a function for an X button X_Button()")
+
+def X_Button():
+    if event == WIN_CLOSED or event == "Exit":
+        while True:
+            break
+
